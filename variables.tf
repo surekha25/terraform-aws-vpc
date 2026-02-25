b9584fcfddda6df726f7d5e4ac7eee1393d1d1af
surekha25 <surekhasuri25@gmail.com>
2026-02-25 21:21:10 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr" {
+    type = string
+    description = "Please provide vpc cidr"
+}
+
+variable "project_name" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "vpc_tags" {
+    type = map 
+    default = {}
+}
